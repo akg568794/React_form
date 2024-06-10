cd625f737af2d14ad78276793ff700302db41afc
abhay gupta <akg568794@gmail.com>
2024-06-10 22:18:15 +0530

    Added the functionality of select country,state and city

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import React,{useState} from 'react'
+import {countries} from "./Data";
 
 export default function App() {
   const [formData,setFormData]=useState({
@@ -9,15 +10,34 @@ export default function App() {
     email:" ",
     password:"",
     phoneNo:"",
-    country:"India",
+    country:"Select Country",
+    state:"",
     city:"",
     panNo:"",
     aadharNo:""
 
   })
+
+  const [states,setStates]=useState([]);
+  const [cities,setCities]=useState([]);
+
   function changeHandler(event){
     const {name,value,checked, type}=event.target;
     setFormData((prev)=>({...prev,[name]:type==="checkbox"? checked:value}))
+  }
+
+  function changeCountry(event){
+    setStates(countries.find(country => country.name===event.target.value).states);
+    setFormData((prev)=>({...prev,country:event.target.value}));
+
+  }
+
+  function changeState(event){
+    setCities(states.find(state=>state.name===event.target.value).cities);
+    setFormData((prev)=>({...prev,state:event.target.value}));
+  }
+  function changeCity(event){
+    setFormData((prev)=>({...prev,city:event.target.value}));
   }
 
   function submitHandler(event){
@@ -26,6 +46,7 @@ export default function App() {
   }
   return (
     <div className='w-full md:max-w-[50%] mx-auto shadow p-8 '>
+      <h1 className='text-center font-bold text-blue-500 text-2xl py-2  '>Registration form</h1>
       <form className="space-y-2" onSubmit={submitHandler}>
       <label className="text-gray-900 leading-6 text-sm font-medium"  htmlFor='firstName'>First Name </label><br/>
       <input className="w-full border mt-2 rounded-md  p-2 text-gray-900 shadow-sm ring-1 placeholder:text-gray-400  placeholder:font-normal"  onChange={changeHandler} type='text' name='firstName' id="firstName" placeholder="first name" value={formData.firstName}></input><br/>
@@ -47,16 +68,33 @@ export default function App() {
       
 
       <label className="text-gray-900 leading-6 text-sm font-medium" htmlFor='country'>Country</label><br/>
-      <select onChange={changeHandler} className="w-full border mt-2 rounded-md  p-2 text-gray-900 shadow-sm ring-1 placeholder:text-gray-400 placeholder:font-normal" value={formData.country} name='country' id='country'>
-      <option value='india'>India</option>
-        <option value='usa'>USA</option>
-        <option value='canada'>Canada</option>
-        <option value='australia'>Australia</option>
-        <option value='uk'>UK</option>
+      <select onChange={changeCountry} className="w-full border mt-2 rounded-md  p-2 text-gray-900 shadow-sm ring-1 placeholder:text-gray-400 placeholder:font-normal" value={formData.country} name='country' id='country'>
+        {
+          countries.map((country)=>{
+            return <option key={country.id} value={country.name}>{country.name}</option>
+          })
+        }
+      </select><br/>
+
+      <label className="text-gray-900 leading-6 text-sm font-medium" htmlFor='state'>State</label><br/>
+      <select onChange={changeState} className="w-full border mt-2 rounded-md  p-2 text-gray-900 shadow-sm ring-1 placeholder:text-gray-400 placeholder:font-normal" value={formData.state} name='state' id='state'>
+        {states.map((state)=>{
+          return <option key={state.id} value={state.name}>{state.name}</option>
+        })}
+      </select><br/>
+
+
+      <label className="text-gray-900 leading-6 text-sm font-medium" htmlFor='city'>City</label><br/>
+      <select onChange={changeCity} className="w-full border mt-2 rounded-md  p-2 text-gray-900 shadow-sm ring-1 placeholder:text-gray-400 placeholder:font-normal" value={formData.city} name='city' id='city'>
+        {
+          cities.map((city)=>{
+            return <option key={city.id} value={city.name}>{city.name}</option>
+          })
+        }
       </select><br/>
 
       <label className="text-gray-900 leading-6 text-sm font-medium" htmlFor='panNo'>Pan-No</label><br/>
-      <input className="w-full border mt-2 rounded-md  p-2 text-gray-900 shadow-sm ring-1 placeholder:text-gray-400 placeholder:font-normal" onChange={changeHandler} type='number' name='panNO' id="panNO" placeholder="ABCTY1234D" value={formData.panNo}></input><br/>
+      <input className="w-full border mt-2 rounded-md  p-2 text-gray-900 shadow-sm ring-1 placeholder:text-gray-400 placeholder:font-normal" onChange={changeHandler} type='text' name='panNo' id="panNo" placeholder="ABCTY1234D" value={formData.panNo}></input><br/>
       
       <label className="text-gray-900 leading-6 text-sm font-medium" htmlFor='aadharNo'>Aadhar-No</label><br/>
       <input className="w-full border mt-2 rounded-md  p-2 text-gray-900 shadow-sm ring-1 placeholder:text-gray-400 placeholder:font-normal" onChange={changeHandler} type='number' name='aadharNo' id="aadharNo" placeholder="2653 8564 4663" value={formData.aadharNo}></input><br/>
---
+++ src/Data.jsx
@@ -0,0 +1,76 @@
+export const countries=[
+        {
+            name:"INDIA",
+            states:[
+                {
+                    name:"Uttar Pradesh",
+                    cities:[
+                        {name:"Prayagraj"},
+                        {name:"Lucknow"},
+                        {name:"Agra"}
+                    ]
+                },
+                {
+                    name:"Maharashtra",
+                    cities:[
+                        {name:"Mumbai"},
+                        {name:"Nagpur"},
+                        {name:"Aurangabad"}
+                    ]
+                },
+                {
+                    name:"Karnataka",
+                    cities:[
+                        {name:"Bengaluru"},
+                        {name:"Udupi"},
+                        {name:"Warangal"}
+                    ]
+                },
+                {
+                    name:"Kerala",
+                    cities:[
+                        {name:"Thiruvananthapuram"},
+                        {name:"Kochi"},
+                        {name:"Kannur"}
+                    ]
+                },
+            ]
+        },
+        {
+            name:"USA",
+            states:[
+                {
+                    name:"California",
+                    cities:[
+                        {name:"Los Angeles"},
+                        {name:"San Diego"},
+                        {name:"San Francisco"}
+                    ]
+                },
+                {
+                    name:"Texas",
+                    cities:[
+                        {name:"Austin"},
+                        {name:"Dallas"},
+                        {name:"Houston"}
+                    ]
+                },
+                {
+                    name:"Florida",
+                    cities:[
+                        {name:"Miami"},
+                        {name:"Orlando"},
+                        {name:"Tampa"}
+                    ]
+                },
+                {
+                    name:"New York",
+                    cities:[
+                        {name:"New York"},
+                        {name:"Albany"},
+                        {name:"Buffalo"}
+                    ]
+                },
+            ]
+        }
+];
